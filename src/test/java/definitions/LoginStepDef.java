401fb070c6980f42f53f4468c2465f452c2ed2e2
IRozinko <ivan.rozinko@gmail.com>
2021-07-26 13:41:58 +0300

    Start

--- src/test/java/definitions/LoginStepDef.java
+++ src/test/java/definitions/LoginStepDef.java
@@ -3,17 +3,11 @@ package definitions;
 import com.shs.actions.UserActions;
 import com.shs.pages.web.PageCollection;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
 public class LoginStepDef {
     PageCollection pages = new PageCollection();
     UserActions doAction = new UserActions();
-
-    @Given("registered client")
-    public void registeredClient() {
-        System.out.println("Register new client client");
-    }
 
     @When("client opens the login page")
     public void clientOpensTheLoginPage() {
